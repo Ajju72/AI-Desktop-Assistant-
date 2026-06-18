2a77940e15ffe66f5a2df2efbcc6fd98eee69246
Ajay Sisodia <ajjusisodia@gmail.com>
2026-06-18 10:36:23 +0530

    Create themes.py

---
+++ ui/themes.py
@@ -0,0 +1,15 @@
+DARK_THEME = """
+QWidget {
+    background-color: #121212;
+    color: white;
+}
+
+QLineEdit {
+    padding: 10px;
+    border-radius: 8px;
+}
+
+QPushButton {
+    padding: 10px;
+}
+"""
